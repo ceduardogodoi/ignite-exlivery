b654fc1d0ed5cc8a45d11460e704a7b4e7193d5c
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2022-12-28 08:40:00 -0300

    Finalizando a struct de Order

--- lib/orders/item.ex
+++ lib/orders/item.ex
@@ -27,5 +27,5 @@ defmodule Exlivery.Orders.Item do
      }}
   end
 
-  defp build_item(:error, _description, _category, _quantity), do: {:error, "Invalid price."}
+  defp build_item(:error, _description, _category, _quantity), do: {:error, "Invalid parameters."}
 end
